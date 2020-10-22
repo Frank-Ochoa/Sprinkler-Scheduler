f4f57e5893393081cb15c3615fb410a734194fc6
Frank-Ochoa <frankieocho@gmail.com>
2020-10-22 16:10:34 -0700

    UI Features complete, maybe auto refresh?

--- src/MaterialTable.js
+++ src/MaterialTable.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import MaterialTable from "material-table";
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import { v4 as uuidv4 } from 'uuid';
 
 
 const Editable = () => {
-	const { useState } = React;
+	const { useState, useEffect } = React;
 	const tableRef = React.createRef();
+	const PORT = 3000;
 
 	const [columns, setColumns] = useState([
 		{ title: 'Scheduled Date/Time', field: 'datetime', type: 'datetime' },
@@ -24,59 +24,23 @@ const Editable = () => {
 		},
 	]);
 
-	// old { sdt: new Date(), sid: 0, str: 60, onOff: 1, uuid: uuidv4()}
-	const [data, setData] = useState([{ datetime: new Date(), sid: 0, str: 60, onoff: 1, reoccur:true, uuid: uuidv4()}]);
-	const [icon, setIcon] = useState(CheckBoxOutlineBlankIcon);
+	const [data, setData] = useState([]);
+	const fetchRemoteData = () =>
+	{
+		fetch(`http://localhost:${PORT}/records`)
+			.then(response => response.json())
+			.then(records => setData(records.data))
+			.catch(err => window.alert('Records Not Properly Fetched'))
+	}
+
+	useEffect(fetchRemoteData, [])
 
 	return (
 		<MaterialTable
 			title="Sprinkler Schedule"
 			columns={columns}
 			tableRef={tableRef}
-			//data={data}
-			data = {query =>
-				new Promise((resolve, reject) => {
-					let url = 'http://localhost:3000/records';
-
-					// if(query.search){
-					// 	console.log(query.search)
-					// 	url += `/filter/${query.search}`;
-					//
-					// 	fetch( url )
-					// 		.then(response => response.json())
-					// 		.then(result => {
-					// 			console.log(result)
-					// 			resolve({
-					// 				data: result.data,
-					// 				page: result.page - 1,
-					// 				totalCount: result.total,
-					// 			})
-					// 		})
-					//
-					// }else
-					// {
-						url += `/per_page/${query.pageSize}/page/${query.page + 1}`
-						fetch( url)
-							.then(response => response.json())
-							.then(result =>
-							{
-								console.log('BEFORE RESOLVE')
-								resolve({
-									data: result.data.filter(function(obj) {
-										return Object.keys(obj).some(function(key) {
-											console.log(`KEY`, key)
-											console.log('VALUE', obj[key])
-											return obj[key] ? (obj[key]).toString().includes(query.search) : false;
-										})
-									}),
-									page: result.page - 1,
-									totalCount: result.total,
-								})
-								console.log('AFTER RESOLVEEEE')
-							})
-					//}
-				})
-			}
+			data = {data}
 			editable={{
 				onRowAdd: newData =>
 					new Promise((resolve, reject) => {
@@ -89,7 +53,8 @@ const Editable = () => {
 								body: JSON.stringify({datetime, sid, str, onoff, reoccur, uuid: uuidv4()})
 							})
 								.then(resp => resp.json())
-								.catch(console.log)
+								.then(record => setData([...data, record]))
+								.catch(err => window.alert('Unable to Schedule Task'))
 
 							resolve();
 						}, 1000)
@@ -97,8 +62,9 @@ const Editable = () => {
 				onRowUpdate: (newData, oldData) =>
 					new Promise((resolve, reject) => {
 						setTimeout(() => {
-							// Will need to cancel the task and reschedule as well as update in the DB
-							console.log('UPDATE DATA', newData)
+							const dataUpdate = [...data];
+							dataUpdate[oldData.tableData.id] = newData;
+
 							const {datetime, sid, str, onoff, reoccur, uuid} = newData;
 
 							fetch('http://localhost:3000/update', {
@@ -107,7 +73,8 @@ const Editable = () => {
 								body: JSON.stringify({datetime, sid, str, onoff, reoccur, uuid})
 							})
 								.then(resp => resp.json())
-								.catch(console.log)
+								.then(() => setData([...dataUpdate]))
+								.catch(err => window.alert('Unable To Update Task'))
 
 							resolve();
 						}, 1000)
@@ -115,11 +82,9 @@ const Editable = () => {
 				onRowDelete: oldData =>
 					new Promise((resolve, reject) => {
 						setTimeout(() => {
-							// const dataDelete = [...data];
-							// const index = oldData.tableData.id;
-							// dataDelete.splice(index, 1);
-							// setData([...dataDelete]);
-
+							const dataDelete = [...data];
+							const index = oldData.tableData.id;
+							dataDelete.splice(index, 1);
 							// Cancel the task, updateDB, probably really only need to send uuid
 							const {uuid} = oldData;
 
@@ -128,8 +93,8 @@ const Editable = () => {
 								headers: {'Content-Type': 'application/json'},
 								body: JSON.stringify({uuid})
 							})
-								.then(resp => resp.json())
-								.catch(console.log)
+								.then(() => setData(dataDelete))
+								.catch(window.alert)
 
 
 							resolve()
@@ -144,7 +109,7 @@ const Editable = () => {
 					icon: 'refresh',
 					tooltip: 'Refresh Data',
 					isFreeAction: true,
-					onClick: () => tableRef.current && tableRef.current.onQueryChange(),
+					onClick: fetchRemoteData,
 				}
 			]}
 		/>
